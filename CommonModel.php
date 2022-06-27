bf53f0e0afa6754268a7d9dd8ca9dee51fd106b7
Bertuğ Fahri ÖZER <bertugozer1994@gmail.com>
2022-06-27 19:00:13 +0300

    codeigniter 4 common model first commit

---
+++ CommonModel.php
@@ -0,0 +1,70 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CommonModel extends Model
+{
+    protected $db;
+
+    public function __construct($group='default')
+    {
+        $this->db=\Config\Database::connect($group);
+    }
+
+    public function lists($table,string $select='*',array $where=[])
+    {
+        $builder=$this->db->table($table);
+        return $builder->select($select)->where($where)->get()->getResult();
+    }
+
+    public function create($table, $data = [])
+    {
+        $builder=$this->db->table($table);
+        $builder->insert($data);
+        return $builder->insertID();
+    }
+
+    public function edit($table, $data = [], $where = [])
+    {
+        $builder=$this->db->table($table);
+        return $builder ->where($where)-> update($data);
+    }
+
+    public function remove($table, $where = [])
+    {
+        $builder=$this->db->table($table);
+        return $builder ->where($where)-> delete();
+    }
+
+    public function selectOne($table,$where = [], $select = '*')
+    {
+        $builder=$this->db->table($table);
+        return $builder -> select($select) -> where($where) -> getRow();
+
+    }
+
+    public function whereInCheckData($att, $where = [], $table)
+    {
+        $builder=$this->db->table($table);
+        return $builder -> whereIn($att, $where) -> getNumRows();
+
+    }
+
+    public function isHave($table,$where)
+    {
+        $builder=$this->db->table($table);
+        return $builder -> getWhere($where,1)->getNumRows();
+    }
+
+    public function count($table, $where = [])
+    {
+        $builder=$this->db->table($table);
+        return $builder -> where($where) -> countAllResults();
+    }
+
+    public function research($like = [], $table, $select = '*', $where = [])
+    {
+        $builder=$this->db->table($table);
+        return $builder -> select($select) -> where($where) -> like($like) -> get() -> getResult();
+    }
+}
